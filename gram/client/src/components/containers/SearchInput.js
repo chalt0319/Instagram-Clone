a9bcc820e08bfb5a6b95174c45703824f3d30735
Courtney <chalt0319@gmail.com>
2018-10-24 15:39:54 -0400

    clear out search bar upon submit

--- gram/client/src/components/containers/SearchInput.js
+++ gram/client/src/components/containers/SearchInput.js
@@ -15,13 +15,16 @@ export default class SearchInput extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     this.props.searchBar(this.state.text)
+    this.setState({
+      text: ""
+    })
   }
 
   render() {
 
     return (
       <form className="search-form" onSubmit={this.handleSubmit}>
-        <input type="text" onChange={this.handleChange} placeholder="keyword"></input><span>  </span>
+        <input type="text" onChange={this.handleChange} placeholder="keyword" value={this.state.text}></input><span>  </span>
         <input type="submit" value="Search"></input>
       </form>
     );
